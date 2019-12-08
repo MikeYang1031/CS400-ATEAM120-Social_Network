f6e02eea5055d3156e701d1534f503bc1f509b9c
yuxiaojia <56175624+yuxiaojia@users.noreply.github.com>
2019-12-07 22:53:28 -0600

    Update Graph.java

--- Graph.java
+++ Graph.java
@@ -18,9 +18,9 @@ public class Graph implements GraphADT {
 
 	private int vertexNumber; // the vertex number in the graph
 	private int edgeNumber;// the edge numebr
-	private HashMap<String, ArrayList<String>> adjList; // main body of graph. array to record vertex, hashmap to record
+	private HashMap<Person, ArrayList<Person>> adjList; // main body of graph. array to record vertex, hashmap to record
 														// edge
-	private ArrayList<String> vertexList;// set the vertex
+	private ArrayList<Person> vertexList;// set the vertex
 	/*
 	 * Default no-argument constructor
 	 */
@@ -28,8 +28,8 @@ public class Graph implements GraphADT {
 	public Graph() {
 		edgeNumber = 0;// initiate edge number
 		vertexNumber = 0;// initiate vertex number
-		adjList = new HashMap<String, ArrayList<String>>(vertexNumber);
-		vertexList = new ArrayList<String>();
+		adjList = new HashMap<Person, ArrayList<Person>>(vertexNumber);
+		vertexList = new ArrayList<Person>();
 	}
 
 	/**
@@ -41,13 +41,13 @@ public class Graph implements GraphADT {
 	 * Valid argument conditions: 1. vertex is non-null 2. vertex is not already in
 	 * the graph
 	 */
-	public void addVertex(String vertex) {
-		if (vertex == null || adjList.containsKey(vertex)) { // vertex is null or aleady in graph
+	public void addVertex(Person person) {
+		if (person == null || adjList.containsKey(person)) { // vertex is null or aleady in graph
 			return;
 		} else {
-			ArrayList<String> edges = new ArrayList<String>();// the vertex's edges
-			adjList.put(vertex, edges); // add the edges list to the vertex
-			vertexList.add(vertex);// add vertex
+			ArrayList<Person> edges = new ArrayList<Person>();// the vertex's edges
+			adjList.put(person, edges); // add the edges list to the vertex
+			vertexList.add(person);// add vertex
 			vertexNumber++;// vertex number increase
 		}
 
@@ -62,35 +62,36 @@ public class Graph implements GraphADT {
 	 * Valid argument conditions: 1. vertex is non-null 2. vertex is not already in
 	 * the graph
 	 */
-	public void removeVertex(String vertex) {
-		if (vertex == null || !adjList.containsKey(vertex)) { // vertex should be non null and in the graph
+	public void removeVertex(Person person) {
+		if (person == null || !adjList.containsKey(person)) { // vertex should be non null and in the graph
 			return;
 		} else {
-
-			for (int i = 0; i < adjList.get(vertex).size(); i++) {
-				adjList.get(vertex).remove(i);// delete the vertex's dependences
+			// useless
+//
+			for (int i = 0; i < adjList.get(person).size(); i++) {
+				adjList.get(person).remove(i);// delete the vertex's relation
 				edgeNumber--;// delete edge number
 			}
-			for (int j = 0; j < vertexList.size(); j++) {// find if any vertex has dependency to this vertex
+			for (int j = 0; j < vertexList.size(); j++) {// find if any vertex has relation to this vertex
 				if (adjList.get(vertexList.get(j)) == null) {
 
-				} else if (adjList.get(vertexList.get(j)).contains(vertex)) {
-					// if any vertex has dependency to this vertex, delete the edge of them
-					//my xteammates help find the errors that I did not delete all of the edges 
-					adjList.get(vertexList.get(j)).remove(vertex);
+				} else if (adjList.get(vertexList.get(j)).contains(person)) {
+					// if any vertex has relationship to this vertex, delete the edge of them
+					// my xteammates help find the errors that I did not delete all of the edges
+					adjList.get(vertexList.get(j)).remove(person);
 					edgeNumber--;
 				}
 			}
 
-			adjList.remove(vertex); // remove edges
-			vertexList.remove(vertex);// remove vertex
+			adjList.remove(person); // remove edges
+			vertexList.remove(person);// remove vertex
 			vertexNumber--;// delete vertex number
 
 		}
 	}
 
 	/**
-	 * Add the edge from vertex1 to vertex2 to this graph. (edge is directed and
+	 * Add the edge from vertex1 to vertex2 to this graph. (edge is undirected and
 	 * unweighted) If either vertex does not exist, add vertex, and add edge, no
 	 * exception is thrown. If the edge exists in the graph, no edge is added and no
 	 * exception is thrown.
@@ -98,46 +99,44 @@ public class Graph implements GraphADT {
 	 * Valid argument conditions: 1. neither vertex is null 2. both vertices are in
 	 * the graph 3. the edge is not in the graph
 	 */
-	public void addEdge(String vertex1, String vertex2) {
-		if (vertex1 == null || vertex2 == null) {// two vertex cannot be null
+	public void addEdge(Person person1, Person person2) {
+		if (person1 == null || person2 == null) {// two vertex cannot be null
 
 			return;
 		} else {
-			if (adjList.containsKey(vertex1)) {// if vertex1 alreay exist
-				if (adjList.get(vertex1).contains(vertex2)) { // vertex2 already in the edge
+			if (adjList.containsKey(person1)) {// if vertex1 alreay exist
+				if (adjList.get(person1).contains(person2)) { // vertex2 already in the edge
 					return;
 				} else {
-					if (adjList.containsKey(vertex2)) {
+					if (adjList.containsKey(person2)) {
 
 					} else {
-						addVertex(vertex2);// add the vertex2 as vertex first
+						addVertex(person2);// add the vertex2 as vertex first
 					}
-					adjList.get(vertex1).add(vertex2);// add vertex2 to edge
+					adjList.get(person1).add(person2);// add vertex2 to edge
 					edgeNumber++;// add the edge number
 				}
 			} else {
-				addVertex(vertex1);// add the vertex1 if vertex1 is not in the graph
-				addEdge(vertex1, vertex2);// recursive call to add the edge
+				addVertex(person1);// add the vertex1 if vertex1 is not in the graph
+				addEdge(person1, person2);// recursive call to add the edge
 			}
 
 		}
 	}
 
 	/**
-	 * Remove the edge from vertex1 to vertex2 from this graph. (edge is directed
-	 * and unweighted) If either vertex does not exist, or if an edge from vertex1
-	 * to vertex2 does not exist, no edge is removed and no exception is thrown.
-	 * 
+	 * Remove the edge from vertex1 to vertex2 from this graph. (edge is undirected
+	 * and unweighted)
 	 * Valid argument conditions: 1. neither vertex is null 2. both vertices are in
 	 * the graph 3. the edge from vertex1 to vertex2 is in the graph
 	 */
-	public void removeEdge(String vertex1, String vertex2) {
-		if (vertex1 == null || vertex2 == null) {
+	public void removeEdge(Person person1, Person person2) {
+		if (person1 == null || person2 == null) {
 			return;
 		} else {
-			if (adjList.containsKey(vertex1)) {
-				if (adjList.get(vertex1).contains(vertex2)) {// if the vertex2 is in the edge
-					adjList.get(vertex1).remove(vertex2);// remove the vertex2 from the edge
+			if (adjList.containsKey(person1)) {
+				if (adjList.get(person1).contains(person2)) {// if the vertex2 is in the edge
+					adjList.get(person1).remove(person2);// remove the vertex2 from the edge
 					edgeNumber--;
 				} else {
 					// if no vertex2, no change
@@ -151,8 +150,8 @@ public class Graph implements GraphADT {
 	 * Returns a Set that contains all the vertices
 	 * 
 	 */
-	public Set<String> getAllVertices() {
-		Set<String> set = new HashSet<>();
+	public Set<Person> getAllVertices() {
+		Set<Person> set = new HashSet<>();
 		if (vertexList == null) {// if there is nothing in the graph
 			return null;
 		} else {
@@ -167,11 +166,11 @@ public class Graph implements GraphADT {
 	 * Get all the neighbor (adjacent) vertices of a vertex
 	 *
 	 */
-	public List<String> getAdjacentVerticesOf(String vertex) {
-		List<String> list = new ArrayList<String>();
-		if (vertexList.contains(vertex)) {// if vertex is in graph
-			for (int i = 0; i < adjList.get(vertex).size(); i++) {
-				list.add(adjList.get(vertex).get(i));// get every edge of the vertex
+	public List<Person> getAdjacentVerticesOf(Person person) {
+		List<Person> list = new ArrayList<Person>();
+		if (vertexList.contains(person)) {// if vertex is in graph
+			for (int i = 0; i < adjList.get(person).size(); i++) {
+				list.add(adjList.get(person).get(i));// get every edge of the vertex
 			}
 		} else {
 			return null;
