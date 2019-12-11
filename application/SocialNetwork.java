d9ffba8acb99440703e5a8e6ebd85e3a86228742
yuxiaojia <56175624+yuxiaojia@users.noreply.github.com>
2019-12-10 21:57:45 -0600

    Update SocialNetwork.java

--- application/SocialNetwork.java
+++ application/SocialNetwork.java
@@ -13,316 +13,320 @@ import java.util.Queue;
 import java.util.Scanner;
 import java.util.Set;
 
-
 /**
  * @author ateam120
  *
  */
 public class SocialNetwork implements SocialNetworkADT {
- ArrayList<String> users = new  ArrayList<String>();
-    private Graph graph;
-    private List<String> tempStore;
-    private Queue<Person> queue;
+	ArrayList<String> users = new ArrayList<String>();
+	private Graph graph;
+	private List<String> tempStore;
+	private Queue<Person> queue;
 	private Person centralUser;
 
-    // constructor
-    public SocialNetwork() {
+	// constructor
+	public SocialNetwork() {
 
-        graph = new Graph();
-        recordOperations = new ArrayList<String>();
+		graph = new Graph();
+		recordOperations = new ArrayList<String>();
 
-    }
+	}
+
 //record every operation made by user
-    private ArrayList<String> recordOperations;
+	private ArrayList<String> recordOperations;
 
-    @Override
-    public boolean addFriends(String person1, String person2) {
- if(!users.contains(person1)) {
-        	addUser(person1);
-        }
-        if(!users.contains(person2)) {
-        	addUser(person2);
-        }
-        Person Person1 = new Person(person1);
-        Person Person2 = new Person(person2);
+	@Override
+	public boolean addFriends(String person1, String person2) {
+		if (!users.contains(person1)) {
+			addUser(person1);
+		}
+		if (!users.contains(person2)) {
+			addUser(person2);
+		}
+	
 
-        graph.addEdge(Person1, Person2);
-        recordOperations.add("a " + person1+ " " + person2);
+		graph.addEdge(person1, person2);
 
-        return true;
-    }
+		recordOperations.add("a " + person1 + " " + person2);
+
+		return true;
+	}
+
+	@Override
+	public boolean removeFriends(String person1, String person2) {
 
 
-    @Override
-    public boolean removeFriends(String person1, String person2) {
+		if (!graph.getAdjacentVerticesOf(person1).contains(person2)) {
+			return false;
+		}
+		graph.removeEdge(person1, person2);
+		recordOperations.add("r " + person1 + " " + person2);
+		return true;
+	}
 
-        Person Person1 = new Person(person1);
-        Person Person2 = new Person(person2);
+	@Override
+	public boolean addUser(String person) {
+		if (users.contains(person)) {
 
-        if (!graph.getAdjacentVerticesOf(Person1).contains(Person2)) {
-            return false;
-        }
-        graph.removeEdge(Person1, Person2);
-        recordOperations.add("r " + person1+ " " + person2);
-        return true;
-    }
+			return false;
+		}
+		users.add(person);
+		graph.addVertex(person);
+		recordOperations.add("a " + person);
+		return true;
+	}
 
-    @Override
-    public boolean addUser(String person) {
-  if(users.contains(person)) {
-        	System.out.println("not add");
-        	return false;
-        }
-        Person Person = new Person(person);
-users.add(person);
-        graph.addVertex(Person);
-        recordOperations.add("a " + person);
-        return true;
-    }
+	@Override
+	public boolean removeUser(String person) {
 
-    @Override
-    public boolean removeUser(String person) {
+		if (!graph.getAllVertices().contains(person)) {
+			return false;
+		}
 
-        if (!graph.getAllVertices().contains(person)) {
-            return false;
-        }
-        Person Person = new Person(person);
+		graph.removeVertex(person);
+		recordOperations.add("r " + person);
+		return true;
+	}
 
-        graph.removeVertex(Person);
-        recordOperations.add("r " + person);
-        return true;
-    }
+	@Override
+	public Set<Person> getFriends(String person) {
 
-    @Override
-    public Set<Person> getFriends(String person) {
+	
 
-        Person Person = new Person(person);
+		List<String> friendsList = graph.getAdjacentVerticesOf(person);
+		Set<Person> friendSet = new HashSet<Person>();
 
-        List<Person> friendsList = graph.getAdjacentVerticesOf(Person);
-        Set<Person> friendSet = new HashSet<Person>();
+		for (String friend : friendsList) {
+			//friendSet.add(friend);
+		}
+		return friendSet;
+	}
 
-        for (Person friend : friendsList) {
-            friendSet.add(friend);
-        }
-        return friendSet;
-    }
+	@Override
+	public Set<Person> getMutualFriends(String person1, String person2) {
 
-    @Override
-    public Set<Person> getMutualFriends(String person1, String person2) {
 
-        Person Person1 = new Person(person1);
-        Person Person2 = new Person(person2);
 
-        List<Person> friend1 = graph.getAdjacentVerticesOf(Person1);
-        List<Person> friend2 = graph.getAdjacentVerticesOf(Person2);
+		List<String> friend1 = graph.getAdjacentVerticesOf(person1);
+		System.out.println(friend1);
+		List<String> friend2 = graph.getAdjacentVerticesOf(person2);
+		if (friend1 == null || friend2 == null) {
+			return null;
+		}
+		Set<Person> mFriend = new HashSet<Person>();
 
-        Set<Person> mFriend = new HashSet<Person>();
+		for (int i = 0; i < friend1.size(); i++) {
+			for (int j = 0; j < friend2.size(); j++) {
 
-        for (int i = 0; i < friend1.size(); i++) {
-            for (int j = 0; j < friend2.size(); j++) {
+				if (friend1.get(i).equals(friend2.get(j))) {
+					//mFriend.add(friend1.get(i));
+					
+					System.out.println(mFriend);
+				}
+			}
+		}
+		return mFriend;
+	}
 
-                if (friend1.get(i).equals(friend2.get(j))) {
-                    mFriend.add(friend1.get(i));
-                }
-            }
-        }
-        return mFriend;
-    }
+	@Override
+	public List<String> getShortestPath(String person1, String person2) {
 
-    @Override
-    public List<String> getShortestPath(String person1, String person2) {
+		Person Person1 = new Person(person1);
+		Person Person2 = new Person(person2);
 
-        Person Person1 = new Person(person1);
-        Person Person2 = new Person(person2);
+		try {
+			tempStore = findPathAlgorithm(Person1, Person2);
+		} catch (Exception e) {
 
-        try {
-            tempStore = findPathAlgorithm(Person1, Person2);
-        } catch (Exception e) {
-            
-            e.printStackTrace();
-        }
-        return tempStore;
-    }
+			e.printStackTrace();
+		}
+		return tempStore;
+	}
 
-    
-    /**
-     * the Dijkstra's Algorithm to find shortest path 
-     * @param person1
-     * @param person2
-     * @return shortest path
-     * @throws Exception 
-     */
-    public List<String> findPathAlgorithm(Person person1, Person person2) throws Exception {
+//	private Person GetPerson(String person) {
+//
+//		return graph.getPerson(person);
+//	}
 
-        if(person1 == null||person2 == null) {
-            throw new NullPointerException("no input,please input person name");
-        }
-        else {
-        
-        tempStore = new ArrayList<String>(); // initializes list to store shortest path
-        Person node = person1; // stores person1 to start
-        person1.totalWeight = 0; // sets start total weight to 0
+	/**
+	 * the Dijkstra's Algorithm to find shortest path
+	 * 
+	 * @param person1
+	 * @param person2
+	 * @return shortest path
+	 * @throws Exception
+	 */
+	public List<String> findPathAlgorithm(Person person1, Person person2) throws Exception {
 
-        queue = new LinkedList<Person>(); // initializes queue
-        queue.add(person1); // adds person1 to queue
+		if (person1 == null || person2 == null) {
+			throw new NullPointerException("no input,please input person name");
+		} else {
 
-        // checks that person2 exists in graph, otherwise throws exception
-        List<Person> vertexList = graph.getVertexList();
-        if (!vertexList.contains(person2))
-            throw new Exception(person2 + " not exist");
+			tempStore = new ArrayList<String>(); // initializes list to store shortest path
+			Person node = person1; // stores person1 to start
+			person1.totalWeight = 0; // sets start total weight to 0
 
-        while (!queue.isEmpty()) {
-            node = queue.remove(); // remove first item from queue
-            node.visited = true; // item has now been visited
+			queue = new LinkedList<Person>(); // initializes queue
+			queue.add(person1); // adds person1 to queue
 
-            // if the person2 has been reached break from loop
-            if (node.getName().equals(person2.getName())) {
-                break;
-                }
-            dhelper(node); // call helper
-        }
+			// checks that person2 exists in graph, otherwise throws exception
+			List<String> vertexList = graph.getVertexList();
+			if (!vertexList.contains(person2))
+				throw new Exception(person2 + " not exist");
 
-        node = person2;
+			while (!queue.isEmpty()) {
+				node = queue.remove(); // remove first item from queue
+				node.visited = true; // item has now been visited
 
-        // copies predecessors of node into list
-        while (node.predecessor != null) {
-            tempStore.add(node.getName());
-            node = node.predecessor;
-        }
-        tempStore.add(person1.getName()); // adds person1 to store
-        Collections.reverse(tempStore); // reverses list so it appears starting at person1, ending at person2
+				// if the person2 has been reached break from loop
+				if (node.getName().equals(person2.getName())) {
+					break;
+				}
+				dhelper(node); // call helper
+			}
 
-        // if store doesn't contain person2 throw exception
-        if (!tempStore.contains(person2.getName()))
-            throw new Exception("Can not find path.");
-        return tempStore;
-        }
-    }
+			node = person2;
 
-    /**
-     * Helper method for dijkstra's algorithm
-     * 
-     * @param person1
-     */
-    private void dhelper(Person person1) {
+			// copies predecessors of node into list
+			while (node.predecessor != null) {
+				tempStore.add(node.getName());
+				node = node.predecessor;
+			}
+			tempStore.add(person1.getName()); // adds person1 to store
+			Collections.reverse(tempStore); // reverses list so it appears starting at person1, ending at person2
 
-        Person node = null;
-        int edgeDistance = -1;
-        int newDistance = -1;
+			// if store doesn't contain person2 throw exception
+			if (!tempStore.contains(person2.getName()))
+				throw new Exception("Can not find path.");
+			return tempStore;
+		}
+	}
 
-        // iterates through list of friends for given person
-        for (int i = 0; i < person1.getListOfUsersFriends().size(); i++) {
-            node = person1.getListOfUsersFriends().get(i);
+	/**
+	 * Helper method for dijkstra's algorithm
+	 * 
+	 * @param person1
+	 */
+	private void dhelper(Person person1) {
 
-            // if person has not been visited yet, check if total weight can be reduced
-            if (!node.visited) {
-                edgeDistance = node.weight;
-                newDistance = person1.totalWeight + edgeDistance;
+		Person node = null;
+		int edgeDistance = -1;
+		int newDistance = -1;
 
-                // if weight can be reduced do it and add person to queue
-                if (newDistance < node.totalWeight) {
-                    node.totalWeight = newDistance;
-                    node.predecessor = person1;
-                    queue.add(node);
-                }
-            }
-        }
-    }
+		// iterates through list of friends for given person
+		for (int i = 0; i < person1.getListOfUsersFriends().size(); i++) {
+			node = person1.getListOfUsersFriends().get(i);
 
-    // This class serves to get the number of disconnected groups in the network
-    @Override
-    public int getConnectedComponents() {
-      
-      int groupNum = 0;
-      List<Person> allNetWorkUsers = getAllUsers();
-      
-      int sizeOfNetwork = allNetWorkUsers.size();
-      
-      boolean [] visitedVertex = new boolean [sizeOfNetwork];
-      
-      for (int i=0; i<sizeOfNetwork; i++) {
-        
-        if (!visitedVertex[sizeOfNetwork]) {
-          
-          DFS(i, visitedVertex, allNetWorkUsers);
-          groupNum++;
-        }
-      }
-      
-      return groupNum;
-    }
-    // this is a private helper class 
-    private void DFS(int i, boolean[] visitedVertices, List<Person>  allUsers) {
-      
-      visitedVertices[i] = true;
-      
-      for (Person x: getFriends(allUsers.get(i).getName())) {
-        int index = allUsers.indexOf(x);
-        if (!visitedVertices[index]) {
-          DFS (index,visitedVertices, allUsers);
-        }
-      }
-    }
-    
-    // this is a private user class to get all the user in the graph and 
-    // then store them in the list
-    private List<Person> getAllUsers () {
-      
-      Set<Person> allUserSet = graph.getAllVertices();
-      ArrayList<Person> usersList = new ArrayList<Person>();
-      
-      for (Person eachUser:allUserSet) {
-        usersList.add(eachUser);
-      }
-      return usersList;
-    }
+			// if person has not been visited yet, check if total weight can be reduced
+			if (!node.visited) {
+				edgeDistance = node.weight;
+				newDistance = person1.totalWeight + edgeDistance;
 
-    @Override 
-    public Person getCentralUser() {
-      return centralUser;
-    }
-    
-    @Override
-    public void setCentral(String setUser) {
-      
-      centralUser.setName(setUser);
-        recordOperations.add("s " + setUser);
-    }
+				// if weight can be reduced do it and add person to queue
+				if (newDistance < node.totalWeight) {
+					node.totalWeight = newDistance;
+					node.predecessor = person1;
+					queue.add(node);
+				}
+			}
+		}
+	}
 
-    @Override
-    public void loadFromFile(File file) {
-    	try {
+	// This class serves to get the number of disconnected groups in the network
+	@Override
+	public int getConnectedComponents() {
+
+		int groupNum = 0;
+		List<String> allNetWorkUsers = getAllUsers();
+
+		int sizeOfNetwork = allNetWorkUsers.size();
+
+		boolean[] visitedVertex = new boolean[sizeOfNetwork];
+
+		for (int i = 0; i < sizeOfNetwork; i++) {
+
+			if (!visitedVertex[sizeOfNetwork]) {
+
+			//	DFS(i, visitedVertex, allNetWorkUsers);
+				groupNum++;
+			}
+		}
+
+		return groupNum;
+	}
+
+	// this is a private helper class
+//	private void DFS(int i, boolean[] visitedVertices, List<String> allNetWorkUsers) {
+//
+//		visitedVertices[i] = true;
+//
+//		for (Person x : getFriends(allNetWorkUsers.get(i).getName())) {
+//			int index = allNetWorkUsers.indexOf(x);
+//			if (!visitedVertices[index]) {
+//				DFS(index, visitedVertices, allNetWorkUsers);
+//			}
+//		}
+//	}
+
+	// this is a private user class to get all the user in the graph and
+	// then store them in the list
+	private List<String> getAllUsers() {
+
+		Set<String> allUserSet = graph.getAllVertices();
+		ArrayList<String> usersList = new ArrayList<String>();
+
+		for (String eachUser : allUserSet) {
+			usersList.add(eachUser);
+		}
+		return usersList;
+	}
+
+	@Override
+	public Person getCentralUser() {
+		return centralUser;
+	}
+
+	@Override
+	public void setCentral(String setUser) {
+
+		centralUser.setName(setUser);
+		recordOperations.add("s " + setUser);
+	}
+
+	@Override
+	public void loadFromFile(File file) {
+		try {
 			Scanner scanner = new Scanner(file);
 			while (scanner.hasNextLine()) {
 				String line = scanner.nextLine();
 				String[] args = line.split(" ");
-				if(args.length == 2) {
-					switch(args[0]) {
-						case "a":
-							addUser(args[1]);
-							System.out.println("added a user "+args[1]);
-							break;
-						case "r":
-							removeUser(args[1]);
-							System.out.println("removed a user "+args[1]);
-							break;
-						case "s":
-							setCentral(args[1]);
-							System.out.println("Set central: "+args[1]);
-							break;
+				if (args.length == 2) {
+					switch (args[0]) {
+					case "a":
+						addUser(args[1]);
+						System.out.println("added a user " + args[1]);
+						break;
+					case "r":
+						removeUser(args[1]);
+						System.out.println("removed a user " + args[1]);
+						break;
+					case "s":
+						setCentral(args[1]);
+						System.out.println("Set central: " + args[1]);
+						break;
 					}
-				}else if(args.length == 3) {
-					switch(args[0]) {
-						case "a":
-							addFriends(args[1],args[2]);
-							System.out.println("added a friendship between "+args[1]+" and "+args[2]);
-							break;
-						case "r":
-							removeFriends(args[1],args[2]);
-							System.out.println("added a friendship between "+args[1]+" and "+args[2]);
-							break;
+				} else if (args.length == 3) {
+					switch (args[0]) {
+					case "a":
+						addFriends(args[1], args[2]);
+						System.out.println("added a friendship between " + args[1] + " and " + args[2]);
+						break;
+					case "r":
+						removeFriends(args[1], args[2]);
+						System.out.println("added a friendship between " + args[1] + " and " + args[2]);
+						break;
 					}
-				}else {
+				} else {
 					// TODO exception
 					System.out.println("This is not a valid command: " + line);
 				}
@@ -330,29 +334,27 @@ users.add(person);
 			scanner.close();
 		} catch (FileNotFoundException e) {
 			e.printStackTrace();
-		} 
-    }
+		}
+	}
 
+	@Override
+	public void saveToFile(File file) {
+		try {
+			FileWriter fileWriter = new FileWriter(file);
+			System.out.println(recordOperations.size());
+			for (int i = 0; i < recordOperations.size(); i++) {
+				String log = recordOperations.get(i);
+				// System.out.println(log);
+				fileWriter.write(log);
+				fileWriter.write("\n");
+			}
+			fileWriter.close();
+		}
 
+		catch (Exception e) {
+			System.out.println("Caution: IOException!");
+		}
 
-    @Override
-    public void saveToFile(File file) {
-        try {
-            FileWriter fileWriter = new FileWriter(file);
-            System.out.println(recordOperations.size());
-            for (int i = 0; i < recordOperations.size(); i++) {
-                String log = recordOperations.get(i);
-            	//System.out.println(log);
-                fileWriter.write(log);
-                fileWriter.write("\n");
-            }
-            fileWriter.close();
-        }
-
-        catch (Exception e) {
-            System.out.println("Caution: IOException!");
-        }
-
-    }
+	}
 
 }
